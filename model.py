19846e7cf2aa725019f9aa492fd6c256390a8154
Wajih Hamrouni <74443451+Wajih24@users.noreply.github.com>
2022-05-08 15:07:14 +0100

    Prediction from json

---
+++ model.py
@@ -0,0 +1,41 @@
+import os
+
+import time
+import traceback
+from sklearn.model_selection import train_test_split 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import *
+from sklearn.cluster import KMeans
+from lightgbm import LGBMClassifier
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+import json
+
+model_directory = 'model'
+model_file_name = '%s/model_player.pickle' % model_directory
+model_columns_file_name = '%s/model_columns_player.pickle' % model_directory
+model_columns = joblib.load(model_columns_file_name)
+clf = joblib.load(model_file_name)
+model_labels = {'Playertype':{'type_achiever': 0,  'type_disruptor': 1,  'type_freeSpirit': 2,   'type_player': 3}}
+
+
+f= open("input.json","r")
+
+input = json.loads(f.read())
+keys = list(input.keys())
+
+# Prediction on all the User_IDs
+for j in keys:
+   sample = {}
+   for i in input[j] :
+      if i in model_columns:
+         sample[i] = input[j][i]
+   query = pd.DataFrame(sample,index=[0])
+   prediction = clf.predict(query)[0]
+   lab = dict((v,k) for k,v in model_labels['Playertype'].items())
+   input[j]["Player_Type"] = lab[prediction] # Add the predcition to the user row
+
+
+with open("output.json", "w") as write_file:
+    json.dump(input, write_file, indent=4)
